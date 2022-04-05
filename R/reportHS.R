94b0b7b38c3f578be78a91df7a65ce83540ee171
Abby Walter <lawalter@mymail.vcu.edu>
2022-04-05 16:45:06 -0400

    reportHS function: added

---
+++ R/reportHS.R
@@ -0,0 +1,65 @@
+#' Write an R markdown report from template
+#'
+#' Create documentation with figures and tables that summarizes Harvest Survey daily and season data.
+#'
+#' @importFrom rmarkdown render
+#'
+#' @param inpath File path to the folder containing HIP .txt files
+#' @param type Type of report. One of the following options may be supplied:
+#' \itemize{
+#' \item survey_analytics - for a summary report of survey analytics
+#' \item season_report - for a summary of the Harvest Survey data}
+#' @param year The hunting season for which Harvest Survey data were collected
+#' @param outpath Folder in which to save the completed report
+#' @param file What the report file should be named
+#' @export
+#' @author Abby Walter, \email{abby_walter@@fws.gov}
+#' @references \url{https://github.com/USFWS/migbirdHarvestData}
+
+reportHS <-
+  function(inpath, type, year, outpath, file){
+    
+    # Create Rmd for download
+    if(type == "survey_analytics"){
+      render(
+        input =
+          # Use the specified template
+          system.file(
+            "templates",
+            paste0(type, ".Rmd"),
+            package = "migbirdMBHS"),
+        # Include the specified parameters so the functions can run
+        params =
+          list(
+            inpath = inpath,
+            outpath = outpath,
+            year = year),
+        output_file = file,
+        output_dir = outpath,
+        # Don't show lengthy knitting status text in console
+        quiet = TRUE)
+    }
+    else if(type == "season_report"){
+      render(
+        input =
+          # Use the specified template
+          system.file(
+            "templates",
+            paste0(type, "_HS", ".Rmd"),
+            package = "migbirdMBHS"),
+        # Include the specified parameters so the functions can run
+        params =
+          list(
+            inpath = inpath,
+            outpath = outpath,
+            year = year),
+        output_file = file,
+        output_dir = outpath,
+        # Don't show lengthy knitting status text in console
+        quiet = TRUE)
+    }
+    else{
+      message("Indicated type must be 'survey_analytics' or 'season_report'.")
+    }
+    
+  }
