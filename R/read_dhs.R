2f942627fb20b4e165c936d616ae5de6b395a9b3
Abby Walter <lawalter@mymail.vcu.edu>
2022-04-05 16:31:13 -0400

    read_dhs function: added an argument to allow this function to work in the new season report template

--- R/read_dhs.R
+++ R/read_dhs.R
@@ -12,37 +12,66 @@
 #'
 #' @param path Directory containing the Harvest Survey daily, season, and/or reference data files
 #' @param year Season for which files should be read in
+#' @param report Default is FALSE; should be used as TRUE in report template only
 #' @export
 #' @author Abby Walter, \email{abby_walter@@fws.gov}
 #' @references \url{https://github.com/USFWS/migbirdMBHS}
 
 read_dhs <-
-  function(path, year){
-    if(year == "all"){
-      list.files(path) %>% 
-        map(~read_csv(paste0(path, .))) %>% 
-        set_names(
-          paste(
-            str_extract(
-              list.files(path), "daily_records|season_totals|all_seasons"),
-            str_extract(list.files(path), "[0-9]{4}"),
-            sep = "_")) %>% 
-        list2env(.GlobalEnv)}
-    else if(year %in% as.character(2019:2100)){
-      list.files(path) %>% 
-        str_subset(year) %>% 
-        map(~read_csv(paste0(path, .))) %>% 
-        set_names(
-          paste(
-            str_extract(
-              list.files(path) %>% 
-                str_subset(year), 
-              "daily_records|season_totals|all_seasons"),
-            year,
-            sep = "_")) %>% 
-        list2env(.GlobalEnv)
+  function(path, year, report = F){
+    if(report == FALSE){
+      if(year == "all"){
+        list.files(path) %>% 
+          map(~read_csv(paste0(path, .))) %>% 
+          set_names(
+            paste(
+              str_extract(
+                list.files(path), "daily_records|season_totals|all_seasons"),
+              str_extract(list.files(path), "[0-9]{4}"),
+              sep = "_")) %>% 
+          list2env(.GlobalEnv)}
+      else if(year %in% as.character(2019:2100)){
+        list.files(path) %>% 
+          str_subset(year) %>% 
+          map(~read_csv(paste0(path, .))) %>% 
+          set_names(
+            paste(
+              str_extract(
+                list.files(path) %>% 
+                  str_subset(year), 
+                "daily_records|season_totals|all_seasons"),
+              year,
+              sep = "_")) %>% 
+          list2env(.GlobalEnv)
+      }
+      else{
+        message("Error: Invalid year.")
+      }
     }
-    else{
-      message("Error: Invalid year.")
+    else if(report == TRUE){
+      if(year == "all"){
+        list.files(path) %>% 
+          map(~read_csv(paste0(path, .))) %>% 
+          set_names(
+            paste(
+              str_extract(
+                list.files(path), "daily_records|season_totals|all_seasons"),
+              str_extract(list.files(path), "[0-9]{4}"),
+              sep = "_")) %>% 
+          list2env(.GlobalEnv)}
+      else if(year %in% as.character(2019:2100)){
+        list.files(path) %>% 
+          str_subset(year) %>% 
+          map(~read_csv(paste0(path, .))) %>% 
+          set_names(
+              str_extract(
+                list.files(path) %>% 
+                  str_subset(year), 
+                "daily_records|season_totals|all_seasons")) %>% 
+          list2env(.GlobalEnv)
+      }
+      else{
+        message("Error: Invalid year.")
+      }
     }
   }
