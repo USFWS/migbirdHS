8a0f729ed26ed32bc1c86fb673ec5e72e25c96a5
Abby Walter <lawalter@mymail.vcu.edu>
2022-04-19 16:43:54 -0400

    correctHS function: added

---
+++ R/correctHS.R
@@ -0,0 +1,40 @@
+#' Correct errors in the Harvest Survey data
+#'
+#' The \code{correct} function processes the errors flagged in \code{proofHS} and returns a final data table.
+#' 
+#' @importFrom stringr str_detect
+#' @importFrom dplyr %>%
+#' @importFrom dplyr mutate
+#' @importFrom dplyr case_when
+#'  
+#' @param data Daily or season data table processed by \code{proofHS}
+#' @export
+#' @author Abby Walter, \email{abby_walter@@fws.gov}
+#' @references \url{https://github.com/USFWS/migbirdMBHS}
+#' 
+
+correctHS <-
+  function(data){
+    if(str_detect(deparse(substitute(data)), "daily") == TRUE){
+      data %>% 
+        mutate(
+          flag = 
+            case_when(
+              # Get rid of records with more than one issue
+              str_detect(errors, "\\-") ~ "discard",
+              # Get rid of records with excessive overbag
+              errors == "overbag" & overbag > 3 ~ "discard",
+              # Get rid of records with excessive overdays
+              errors == "overdays" & overday > 3 ~ "discard",
+              errors == "late_hunt" ~ "late",
+              errors == "early_hunt" ~ "early",
+              TRUE ~ NA_character_))
+    }
+    else if(str_detect(deparse(substitute(data)), "season") == TRUE){
+      data
+    }
+    else{
+      message(
+        "Error: `data` must be season_totals or daily_records data frame.")
+    }
+  }
