8dc705522e17a46bd3021f871c790a0db93a7713
lawalter <abby_walter@fws.gov>
2023-06-30 08:52:35 -0400

    add_seaducks function: overhaul of function code

--- R/add_seaducks.R
+++ R/add_seaducks.R
@@ -1,92 +1,43 @@
-#' Append sea duck information to the Harvest Survey reference table
+#' Add sea duck dates for eligible states
 #'
-#' The \code{add_seaducks} function includes the sea duck open date, close date, and bag limit for California and Washington in the reference table. The California and Washington sea duck seasons match the same dates and bags as the states' duck seasons.
-#'
-#' @importFrom dplyr %>%
+#' The \code{add_seaducks} function amends the ref_data for AK, CT, DE, ME, MD, MA, NH, NJ, NY, OR, RI, VA, WA, and CA by adding lines to account for seaduck seasons; it uses the same values as duck harvest for open, close, bag, and possession.
+#' 
 #' @importFrom dplyr bind_rows
-#' @importFrom purrr map_dfc
-#' @importFrom dplyr mutate
-#' @importFrom readr type_convert
-#' @importFrom dplyr case_when
 #' @importFrom dplyr filter
-#' @importFrom dplyr select
-#' @importFrom dplyr pull
-#' @importFrom stats setNames
-#'
-#' @param ref_data A reference data table
+#' @importFrom dplyr mutate
+#' @importFrom dplyr distinct
+#' @importFrom dplyr group_by
+#' @importFrom dplyr ungroup
+#' 
+#' @param ref_data The reference data table that corresponds to the year of the daily data
 #' @export
 #' @author Abby Walter, \email{abby_walter@@fws.gov}
 #' @references \url{https://github.com/USFWS/migbirdMBHS}
+#' 
 
 add_seaducks <-
-  function(ref_data){
+  function(ref_data) {
+    
+    ref_data |> 
     bind_rows(
-      ref_data, 
-      bind_rows(
-        names(ref_data) %>% 
-          map_dfc(setNames, object = list(character(1))) %>% 
-          mutate(
-            SeasonType = "Regular",
-            ST = "CA",
-            SpeciesGroup = "Sea ducks",
-            Species = "Sea ducks"),
-        names(ref_data) %>% 
-          map_dfc(setNames, object = list(character(1))) %>% 
-          mutate(
-            SeasonType = "Regular",
-            ST = "WA",
-            SpeciesGroup = "Sea ducks",
-            Species = "Sea ducks")) %>% 
-        type_convert()) %>% 
-      mutate(
-        Open = 
-          case_when(
-            ST == "CA" & Species == "Sea ducks" ~ 
-              ref_data %>% 
-              filter(ST == "CA" & SpeciesGroup == "Ducks") %>% 
-              select(Open) %>% 
-              pull() %>% 
-              min(., na.rm = T) %>% 
-              as.character(),
-            ST == "WA" & Species == "Sea ducks" ~ 
-              ref_data %>% 
-              filter(ST == "WA" & SpeciesGroup == "Ducks") %>% 
-              select(Open) %>% 
-              pull() %>% 
-              min(., na.rm = T) %>% 
-              as.character(),
-            TRUE ~ Open),
-        Close = 
-          case_when(
-            ST == "CA" & Species == "Sea ducks" ~ 
-              ref_data %>% 
-              filter(ST == "CA" & SpeciesGroup == "Ducks") %>% 
-              select(Close) %>% 
-              pull() %>% 
-              max(., na.rm = T) %>% 
-              as.character(),
-            ST == "WA" & Species == "Sea ducks" ~ 
-              ref_data %>% 
-              filter(ST == "WA" & SpeciesGroup == "Ducks") %>% 
-              select(Close) %>% 
-              pull() %>% 
-              max(., na.rm = T) %>% 
-              as.character(),
-            TRUE ~ Close),
-        Bag = 
-          case_when(
-            ST == "CA" & Species == "Sea ducks" ~ 
-              ref_data %>% 
-              filter(ST == "CA" & SpeciesGroup == "Ducks") %>% 
-              select(Bag) %>% 
-              pull() %>% 
-              max(., na.rm = T),
-            ST == "WA" & Species == "Sea ducks" ~ 
-              ref_data %>% 
-              filter(ST == "WA" & SpeciesGroup == "Ducks") %>% 
-              select(Bag) %>% 
-              pull() %>% 
-              max(., na.rm = T),
-            TRUE ~ Bag)
-      ) 
+      ref_data |> 
+        filter(
+          ST %in% c("AK", "CT", "DE", "ME", "MD", "MA", "NH", 
+                    "NJ", "NY", "OR", "RI", "VA", "WA", "CA") &
+            SpeciesGroup == "Ducks") |> 
+        mutate(
+          SeasonType = "Reg",
+          SpeciesGroup = "Sea ducks",
+          Species = "Sea ducks") |> 
+        distinct() |> 
+        group_by(ST) |> 
+        mutate(
+          Open = min(Open, na.rm = T),
+          Close = max(Close, na.rm = T),
+          Bag = max(Bag, na.rm = T),
+          Possession = max(Possession, na.rm = T)
+        ) |> 
+        ungroup() |> 
+        distinct()
+      )
   }
